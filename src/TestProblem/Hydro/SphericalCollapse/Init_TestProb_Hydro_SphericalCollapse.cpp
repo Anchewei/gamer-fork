b6184cad1c946893c7b0228d7470c6fe62050bfc
Anchewei <10302leo@gmail.com>
2023-05-25 16:14:40 +0800

    almost forget SphericalCollapse

--- src/TestProblem/Hydro/SphericalCollapse/Init_TestProb_Hydro_SphericalCollapse.cpp
+++ src/TestProblem/Hydro/SphericalCollapse/Init_TestProb_Hydro_SphericalCollapse.cpp
@@ -5,14 +5,60 @@
 
 // problem-specific global variables
 // =======================================================================================
-static double SphCol_Dens_Bg;       // background mass density
-static double SphCol_Dens_Delta;    // top-hat mass density --> total density = Dens_Bg*( 1 + Dens_Delta )
-static double SphCol_Engy_Bg;       // background energy density
-static double SphCol_Radius;        // top-hat radius
-static double SphCol_Center[3];     // top-hat center
+static double     *tur_table = NULL;              // used to store turbulence (1D)
+static int        tur_table_NBin;                 // number of row in turbulence table obtained by Aux_LoadTable
+static int        tur_table_Ncol;                 // number of column in turbulence table (set by user)
+static int        *CF_TargetCols = new int [6];    // Index of columns read from the turbulence table 
+static int        CF_ColIdx_X;                    // Column index of x coordinate in the turbulence table
+static int        CF_ColIdx_Y;                    // Column index of y coordinate in the turbulence table 
+static int        CF_ColIdx_Z;                    // Column index of z coordinate in the turbulence table 
+static int        CF_ColIdx_VelX;                 // Column index of x direction velocity in the turbulence table 
+static int        CF_ColIdx_VelY;                 // Column index of y direction velocity in the turbulence table 
+static int        CF_ColIdx_VelZ;                 // Column index of z direction velocity in the turbulence table 
+static double     *Table_X;                       // used to store the readed data
+static double     *Table_Y;
+static double     *Table_Z;
+static double     *Table_VelX;
+static double     *Table_VelY;
+static double     *Table_VelZ;
+static int        size;                           // turbulence cell number
+static double     Total_VelX;                     // used to calculate Vrms
+static double     Total_VelY;
+static double     Total_VelZ;
+static double     Total_VelX_SQR;
+static double     Total_VelY_SQR;
+static double     Total_VelZ_SQR;
+static double     Vrms;
+static double     Vrms_Scale;                     // used to rescale velocity
+static int        Total_Vrms_Count;
+
+static double     Cs;                             // sound spped
+
+static char       CF_Tur_Table[MAX_STRING];
+static double     CF_vflow;
+double            rho_AD_SC;                      // adiabatic density thresheld for barotropic EOS
+double            rho_PW_SC;                      // adiabatic density thresheld for piecewise EOS
+static int        Plummer_like;                   // use Plummer like density profile
+static int        EOS_type;                       // 0: barotropic EOS, 1: piecewise EOS
+
+static double     Mach;
+static double     Rho0;
+static double     R0;
+static double     Omega0;
+static double     Core_Mass;
+static double     Delta_Dens;
+static double     Dens_Contrast;
+static double     Inner_R0;                      // for Plummer_like
+static double     Rho0_P;                        // for Plummer_like
 // =======================================================================================
 
-
+#ifdef FEEDBACK
+void FB_Init_SinkAccretion();
+#endif
+#  if ( EOS == EOS_USER )
+void EoS_Init_Barotropic_SC();
+void EoS_Init_Piecewise_SC();
+#  endif
 
 
 //-------------------------------------------------------------------------------------------------------
@@ -39,13 +85,18 @@ void Validate()
    Aux_Error( ERROR_INFO, "GRAVITY must be enabled !!\n" );
 #  endif
 
-#  ifndef COMOVING
-   Aux_Error( ERROR_INFO, "COMOVING must be enabled !!\n" );
+#  ifdef PARTICLE
+   if ( OPT__INIT == INIT_BY_FUNCTION  &&  amr->Par->Init != PAR_INIT_BY_FUNCTION )
+      Aux_Error( ERROR_INFO, "please set PAR_INIT = 1 (by FUNCTION) !!\n" );
 #  endif
 
-#  ifdef PARTICLE
-   Aux_Error( ERROR_INFO, "PARTICLE must be disabled !!\n" );
-#  endif
+// #  ifndef COMOVING
+//    Aux_Error( ERROR_INFO, "COMOVING must be enabled !!\n" );
+// #  endif
+
+// #  ifdef PARTICLE
+//    Aux_Error( ERROR_INFO, "PARTICLE must be disabled !!\n" );
+// #  endif
 
 #  ifdef GRAVITY
    if ( !OPT__SELF_GRAVITY )
@@ -116,25 +167,67 @@ void SetParameter()
 // ********************************************************************************************************************************
 // ReadPara->Add( "KEY_IN_THE_FILE",   &VARIABLE,              DEFAULT,       MIN,              MAX               );
 // ********************************************************************************************************************************
-   ReadPara->Add( "SphCol_Dens_Bg",    &SphCol_Dens_Bg,        -1.0,          Eps_double,       NoMax_double      );
-   ReadPara->Add( "SphCol_Dens_Delta", &SphCol_Dens_Delta,     -1.0,          Eps_double,       NoMax_double      );
-   ReadPara->Add( "SphCol_Engy_Bg",    &SphCol_Engy_Bg,        -1.0,          Eps_double,       NoMax_double      );
-   ReadPara->Add( "SphCol_Radius",     &SphCol_Radius,         -1.0,          Eps_double,       NoMax_double      );
-   ReadPara->Add( "SphCol_Center_X",   &SphCol_Center[0],      -1.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "SphCol_Center_Y",   &SphCol_Center[1],      -1.0,          NoMin_double,     NoMax_double      );
-   ReadPara->Add( "SphCol_Center_Z",   &SphCol_Center[2],      -1.0,          NoMin_double,     NoMax_double      );
+   ReadPara->Add( "CF_Tur_Table",      CF_Tur_Table,           NoDef_str,     Useless_str,      Useless_str       );
+   ReadPara->Add( "Mach",              &Mach,                  0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "CF_vflow",          &CF_vflow,              0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "R0",                &R0,                    0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Omega0",            &Omega0,                0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Core_Mass",         &Core_Mass,             0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Delta_Dens",        &Delta_Dens,            0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Dens_Contrast",     &Dens_Contrast,         0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "EOS_type",          &EOS_type,              0,             0,                NoMax_int         );
+   ReadPara->Add( "rho_AD_SC",         &rho_AD_SC,             0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "rho_PW_SC",         &rho_PW_SC,             0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Plummer_like",      &Plummer_like,            0,             0,              NoMax_int         );
+   ReadPara->Add( "Inner_R0",          &Inner_R0,              0.0,           0.0,              NoMax_double      );
+   ReadPara->Add( "Rho0_P",            &Rho0_P,                0.0,           0.0,              NoMax_double      );
 
    ReadPara->Read( FileName );
 
    delete ReadPara;
 
-// set the default values
-   for (int d=0; d<3; d++)
-      if ( SphCol_Center[d] < 0.0 )    SphCol_Center[d] = 0.5*amr->BoxSize[d];
-
 
 // (2) set the problem-specific derived parameters
+   tur_table_Ncol = 6;
+   CF_TargetCols[0] =  0;
+   CF_TargetCols[1] =  1;
+   CF_TargetCols[2] =  2;
+   CF_TargetCols[3] =  3;
+   CF_TargetCols[4] =  4;
+   CF_TargetCols[5] =  5;
+   CF_ColIdx_X      =  0;
+   CF_ColIdx_Y      =  1;
+   CF_ColIdx_Z      =  2;
+   CF_ColIdx_VelX   =  3;
+   CF_ColIdx_VelY   =  4;
+   CF_ColIdx_VelZ   =  5;
+   Total_VelX = 0.0;
+   Total_VelY = 0.0;
+   Total_VelZ = 0.0;
+   Total_VelX_SQR = 0.0;
+   Total_VelY_SQR = 0.0;
+   Total_VelZ_SQR = 0.0;
+   Vrms = 0.0;
+   Vrms_Scale = 0.0;
+   Total_Vrms_Count = 0;
+   size = 129;
 
+   Core_Mass *= Const_Msun;
+   Cs = SQRT( ( Const_kB*ISO_TEMP/UNIT_E ) / ( MOLECULAR_WEIGHT*Const_amu/UNIT_M ));
+   R0 /= UNIT_L;
+
+   if ( Plummer_like == 1)
+   {
+      Rho0 = Rho0_P/UNIT_D;
+   }
+   else
+   {
+      Rho0 = 3.0 * Core_Mass / (4.0 * M_PI * CUBE(R0));
+   }
+
+
+   Omega0 /= 1/UNIT_T;
+   rho_AD_SC /= UNIT_D;
 
 // (3) reset other general-purpose parameters
 //     --> a helper macro PRINT_WARNING is defined in TestProb.h
@@ -157,13 +250,30 @@ void SetParameter()
    {
       Aux_Message( stdout, "=============================================================================\n" );
       Aux_Message( stdout, "  test problem ID           = %d\n",     TESTPROB_ID       );
-      Aux_Message( stdout, "  background mass density   = %13.7e\n", SphCol_Dens_Bg    );
-      Aux_Message( stdout, "  top-hat over-density      = %13.7e\n", SphCol_Dens_Delta );
-      Aux_Message( stdout, "  background energy density = %13.7e\n", SphCol_Engy_Bg    );
-      Aux_Message( stdout, "  top-hat radius            = %13.7e\n", SphCol_Radius     );
-      Aux_Message( stdout, "  top-hat center x          = %13.7e\n", SphCol_Center[0]  );
-      Aux_Message( stdout, "  ...            y          = %13.7e\n", SphCol_Center[1]  );
-      Aux_Message( stdout, "  ...            z          = %13.7e\n", SphCol_Center[2]  );
+      Aux_Message( stdout, "  Mach number           = %13.7e \n",       Mach                                 );
+      Aux_Message( stdout, "  Flow velocity         = %13.7e km/s\n",   CF_vflow                             );
+      Aux_Message( stdout, "  Sound speed           = %13.7e km/s\n",   Cs*UNIT_V/Const_km                   );
+      Aux_Message( stdout, "  Turbulence table      = %s\n",            CF_Tur_Table                         );
+      Aux_Message( stdout, "  Plummer_like          = %d \n",           Plummer_like                         );
+      Aux_Message( stdout, "  R0                    = %13.7e cm\n",     R0*UNIT_L                            );
+      Aux_Message( stdout, "  Rho0                  = %13.7e g/cm3\n",  Rho0*UNIT_D                          );
+      Aux_Message( stdout, "  Omega0                = %13.7e /s\n",     Omega0*UNIT_T                        );
+      Aux_Message( stdout, "  Core Mass             = %13.7e M_sun\n",  Core_Mass/Const_Msun                 );
+      Aux_Message( stdout, "  EOS_type              = %d \n",           EOS_type                             );
+
+      if ( EOS_type == 0 )
+      {
+         Aux_Message( stdout, "  rho_AD_SC             = %13.7e \n",       rho_AD_SC                         );
+      }
+      else
+      {
+         Aux_Message( stdout, "  rho_PW_SC             = %13.7e \n",       rho_PW_SC                         );
+      }
+
+
+      if ( Plummer_like == 1)
+      Aux_Message( stdout, "  Inner_R0              = %13.7e \n",       Inner_R0                             );
+
       Aux_Message( stdout, "=============================================================================\n" );
    }
 
@@ -172,7 +282,47 @@ void SetParameter()
 
 } // FUNCTION : SetParameter
 
+//-------------------------------------------------------------------------------------------------------
+// Function    :  Load_Turbulence
+// Description : load turbulence and calculate Vrms_Scale
+// Note        :
+// Parameter   :
+// Return      :
+//-------------------------------------------------------------------------------------------------------
+void Load_Turbulence_SC()
+{
+   const bool RowMajor_No  = false;           // load data into the column major
+   const bool AllocMem_Yes = true;            // allocate memory for ISM_Velocity_Perturbation
+   const double BoxSize[3]   = { amr->BoxSize[0], amr->BoxSize[1], amr->BoxSize[2] };
+   const double BoxCenter[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
 
+   tur_table_NBin = Aux_LoadTable( tur_table, CF_Tur_Table, tur_table_Ncol, CF_TargetCols, RowMajor_No, AllocMem_Yes );
+
+   Table_X     = tur_table + CF_ColIdx_X * tur_table_NBin;
+   Table_Y     = tur_table + CF_ColIdx_Y * tur_table_NBin;
+   Table_Z     = tur_table + CF_ColIdx_Z * tur_table_NBin;
+   Table_VelX  = tur_table + CF_ColIdx_VelX * tur_table_NBin;
+   Table_VelY  = tur_table + CF_ColIdx_VelY * tur_table_NBin;
+   Table_VelZ  = tur_table + CF_ColIdx_VelZ * tur_table_NBin;
+
+   for ( int i = 0; i < tur_table_NBin; i++ )
+   {
+      Total_VelX += Table_VelX[i];
+      Total_VelY += Table_VelY[i];
+      Total_VelZ += Table_VelZ[i];
+
+      Total_VelX_SQR += SQR(Table_VelX[i]);
+      Total_VelY_SQR += SQR(Table_VelY[i]);
+      Total_VelZ_SQR += SQR(Table_VelZ[i]);
+
+      Total_Vrms_Count ++;
+   }
+
+   // Vrms = SQRT( ( Vx^2 + Vy^2 + Vz^2 ) / N + ( Vx + Vy + Vz / N) ^ 2 )
+   Vrms = SQRT( (Total_VelX_SQR + Total_VelY_SQR + Total_VelZ_SQR) / Total_Vrms_Count - 
+                SQR( (Total_VelX + Total_VelY + Total_VelZ) / Total_Vrms_Count ) );
+   Vrms_Scale = Mach * Cs / Vrms;
+} // Function : Load_Turbulence
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  SetGridIC
@@ -196,19 +346,86 @@ void SetParameter()
 void SetGridIC( real fluid[], const double x, const double y, const double z, const double Time,
                 const int lv, double AuxArray[] )
 {
+   const double BoxSize[3]   = { amr->BoxSize[0], amr->BoxSize[1], amr->BoxSize[2] };
+   const double BoxCenter[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
+   const double dx = x - BoxCenter[0];
+   const double dy = y - BoxCenter[0];
+   const double dz = z - BoxCenter[0];
+   const double Rc = SQRT( SQR(dx) + SQR(dy) );
+   const double Rs = SQRT( SQR(dx) + SQR(dy) + SQR(dz) );
 
-   const double r = sqrt( SQR(x-SphCol_Center[0]) + SQR(y-SphCol_Center[1]) + SQR(z-SphCol_Center[2]) );
+   double Dens, MomX, MomY, MomZ, Pres, Eint, Etot;
+   double VelX, VelY, VelZ;
 
-   fluid[DENS] = SphCol_Dens_Bg;
-   fluid[MOMX] = 0.0;
-   fluid[MOMY] = 0.0;
-   fluid[MOMZ] = 0.0;
-   fluid[ENGY] = SphCol_Engy_Bg;
+   if ( Mach != 0.0 )
+   {
+      int i = (int) ( ( x / BoxSize[0] ) * size );
+      int j = (int) ( ( y / BoxSize[0] ) * size );
+      int k = (int) ( ( z / BoxSize[0] ) * size );
+      int index = i * SQR(size) + j * size + k;
+      if ( i < 0 || i > size ) Aux_Error( ERROR_INFO, "index is out of bound\n, i = %d", i );
+      if ( j < 0 || j > size ) Aux_Error( ERROR_INFO, "index is out of bound\n, j = %d", j );
+      if ( k < 0 || k > size ) Aux_Error( ERROR_INFO, "index is out of bound\n, k = %d", k );
+      if ( index < 0 || index > tur_table_NBin ) Aux_Error( ERROR_INFO, "index is out of bound\n, index = %d", index );
+      VelX = Vrms_Scale * ( Table_VelX[ index ] - Total_VelX / Total_Vrms_Count );
+      VelY = Vrms_Scale * ( Table_VelY[ index ] - Total_VelY / Total_Vrms_Count );
+      VelZ = Vrms_Scale * ( Table_VelZ[ index ] - Total_VelZ / Total_Vrms_Count );
+   }
 
-   if ( r <= SphCol_Radius )  fluid[DENS] *= ( 1.0 + SphCol_Dens_Delta );
+   if ( Rs < R0 )
+   {
+      if ( Plummer_like == 1)
+      {
+         Dens = Rho0 / ( SQR(Rs/Inner_R0) + 1 );
+      }
+      else
+      {
+         Dens = Rho0 * (1 + Delta_Dens * COS(2 * ATAN(dy/dx)));
+      }
+      VelX -= Omega0 * dy;
+      VelY += Omega0 * dx;
+   }
+   else
+   {
+      if ( Plummer_like == 1)
+      {
+         Dens = (Rho0 / ( SQR(R0/Inner_R0) + 1 )) / Dens_Contrast;
+      }
+      else
+      {
+         Dens = Rho0 / Dens_Contrast;
+      }
+   }
+
+   Pres = EoS_DensTemp2Pres_CPUPtr( Dens, ISO_TEMP, NULL, EoS_AuxArray_Flt,
+                                    EoS_AuxArray_Int, h_EoS_Table );
+   Eint = EoS_DensPres2Eint_CPUPtr( Dens, Pres, NULL, EoS_AuxArray_Flt,
+                                    EoS_AuxArray_Int, h_EoS_Table );
+   // Eint = EoS_DensPres2Eint_CPUPtr( Dens, Dens * SQR(Cs), NULL, EoS_AuxArray_Flt,
+   //                                  EoS_AuxArray_Int, h_EoS_Table );
+   MomX = Dens * VelX;
+   MomY = Dens * VelY;
+   MomZ = Dens * VelZ;
+   Etot = Hydro_ConEint2Etot( Dens, MomX, MomY, MomZ, Eint, 0.0 );     // do NOT include magnetic energy here
+
+   fluid[DENS] = Dens;
+   fluid[MOMX] = MomX;
+   fluid[MOMY] = MomY;
+   fluid[MOMZ] = MomZ;
+   fluid[ENGY] = Etot;
 
 } // FUNCTION : SetGridIC
 #endif // #if ( MODEL == HYDRO )
+
+#  ifdef PARTICLE
+void Par_Init_ByFunction_SC( const long NPar_ThisRank, const long NPar_AllRank,
+                          real *ParMass, real *ParPosX, real *ParPosY, real *ParPosZ,
+                          real *ParVelX, real *ParVelY, real *ParVelZ, real *ParTime,
+                          real *ParType, real *AllAttribute[PAR_NATT_TOTAL] )
+{
+   // we only use star particles, so keep here empty.
+}  // FUNCTION : Par_Init_ByFunction
+#  endif
 
 
 
@@ -235,11 +452,31 @@ void Init_TestProb_Hydro_SphericalCollapse()
 #  if ( MODEL == HYDRO )
 // set the problem-specific runtime parameters
    SetParameter();
+   if ( OPT__INIT != INIT_BY_RESTART ) Load_Turbulence_SC();
 
 
 // set the function pointers of various problem-specific routines
    Init_Function_User_Ptr = SetGridIC;
 #  endif // #if ( MODEL == HYDRO )
+#  ifdef PARTICLE
+   Par_Init_ByFunction_Ptr           = Par_Init_ByFunction_SC; // option: PAR_INIT=1;              example: Particle/Par_Init_ByFunction.cpp
+   Par_Init_Attribute_User_Ptr       = NULL; // set PAR_NATT_USER;               example: TestProblem/Hydro/AGORA_IsolatedGalaxy/Init_TestProb_Hydro_AGORA_IsolatedGalaxy.cpp --> AddNewParticleAttribute()
+#  endif
+#  ifdef FEEDBACK
+   FB_Init_User_Ptr        = FB_Init_SinkAccretion;
+#  endif
+#  if ( EOS == EOS_USER )
+   if ( EOS_type == 0 )
+   {
+      EoS_Init_Ptr                      = EoS_Init_Barotropic_SC; // option: EOS in the Makefile;     example: EoS/User_Template/CPU_EoS_User_Template.cpp
+   }
+   else
+   {
+      EoS_Init_Ptr                      = EoS_Init_Piecewise_SC; // option: EOS in the Makefile;     example: EoS/User_Template/CPU_EoS_User_Template.cpp
+   }
+
+   EoS_End_Ptr                       = NULL;
+#  endif
 
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ... done\n", __FUNCTION__ );
