e9744541179f3e94676b1a15490dec5fb1dd3a5d
Hsi-Yu Schive <hyschive@gmail.com>
2026-04-21 18:09:48 +0800

    Minor

--- include/Prototype.h
+++ include/Prototype.h
@@ -771,7 +771,7 @@ void Par_SetParUID();
 void Par_Init_Attribute_Mesh();
 void Par_Output_TracerParticle_Mesh();
 void Par_SetFlag( const int Flag );
-bool Par_Flag_TargetParticle( const int FaLv, const int PID, const ParFlag_t FlagMode );
+bool Par_Flag_TargetParticle( const int lv, const int PID, const ParFlag_t FlagMode );
 FieldIdx_t AddParticleAttributeFlt( const char *InputLabel );
 FieldIdx_t AddParticleAttributeInt( const char *InputLabel );
 FieldIdx_t GetParticleAttributeFltIndex( const char *InputLabel, const Check_t Check );
