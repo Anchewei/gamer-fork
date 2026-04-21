ff120187552e9143495720ae53ecb9f9231eee7b
Hsi-Yu Schive <hyschive@gmail.com>
2026-04-21 16:37:10 +0800

    Add the runtime parameter OPT__FLAG_PAR_TARGET_SIB

--- src/Output/Output_DumpData_Total_HDF5.cpp
+++ src/Output/Output_DumpData_Total_HDF5.cpp
@@ -287,7 +287,7 @@ Procedure for outputting new variables:
 //                                             OPT__OUTPUT_GRACKLE_TEMP, OPT__OUTPUT_GRACKLE_MU, OPT__OUTPUT_GRACKLE_TCOOL,
 //                                             DT__GRACKLE_COOLING, OPT__FLAG_COOLING_LEN, FlagTable_CoolingLen
 //                2508 : 2026/03/26 --> output particle unique id
-//                2509 : 2026/04/18 --> output OPT__FLAG_PAR_TARGET, Par->FlagInit
+//                2509 : 2026/04/18 --> output OPT__FLAG_PAR_TARGET, OPT__FLAG_PAR_TARGET_SIB, Par->FlagInit
 //-------------------------------------------------------------------------------------------------------
 void Output_DumpData_Total_HDF5( const char *FileName )
 {
@@ -2628,6 +2628,7 @@ void FillIn_InputPara( InputPara_t &InputPara, const int NFieldStored, char Fiel
    InputPara.Opt__Flag_NParCell      = OPT__FLAG_NPAR_CELL;
    InputPara.Opt__Flag_ParMassCell   = OPT__FLAG_PAR_MASS_CELL;
    InputPara.Opt__Flag_ParTarget     = OPT__FLAG_PAR_TARGET;
+   InputPara.Opt__Flag_ParTargetSib  = OPT__FLAG_PAR_TARGET_SIB;
 #  endif
    InputPara.Opt__NoFlagNearBoundary = OPT__NO_FLAG_NEAR_BOUNDARY;
    InputPara.Opt__PatchCount         = OPT__PATCH_COUNT;
@@ -3720,6 +3721,7 @@ void GetCompound_InputPara( hid_t &H5_TypeID, const int NFieldStored )
    H5Tinsert( H5_TypeID, "Opt__Flag_NParCell",      HOFFSET(InputPara_t,Opt__Flag_NParCell     ), H5T_NATIVE_INT     );
    H5Tinsert( H5_TypeID, "Opt__Flag_ParMassCell",   HOFFSET(InputPara_t,Opt__Flag_ParMassCell  ), H5T_NATIVE_INT     );
    H5Tinsert( H5_TypeID, "Opt__Flag_ParTarget",     HOFFSET(InputPara_t,Opt__Flag_ParTarget    ), H5T_NATIVE_INT     );
+   H5Tinsert( H5_TypeID, "Opt__Flag_ParTargetSib",  HOFFSET(InputPara_t,Opt__Flag_ParTargetSib ), H5T_NATIVE_INT     );
 #  endif
    H5Tinsert( H5_TypeID, "Opt__NoFlagNearBoundary", HOFFSET(InputPara_t,Opt__NoFlagNearBoundary), H5T_NATIVE_INT     );
    H5Tinsert( H5_TypeID, "Opt__PatchCount",         HOFFSET(InputPara_t,Opt__PatchCount        ), H5T_NATIVE_INT     );
