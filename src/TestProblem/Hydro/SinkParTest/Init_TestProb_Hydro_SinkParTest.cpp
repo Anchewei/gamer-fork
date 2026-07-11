4908d63190daaac410980c2382de09cddf6c2511
Anchewei <10302leo@gmail.com>
2026-07-11 17:30:14 +0800

    refactor: improve code readability and consistency by formatting and cleaning up whitespace

--- src/TestProblem/Hydro/SinkParTest/Init_TestProb_Hydro_SinkParTest.cpp
+++ src/TestProblem/Hydro/SinkParTest/Init_TestProb_Hydro_SinkParTest.cpp
@@ -21,10 +21,9 @@ static double SinkParTest_Delta_Dens;    // The density of the cloud
 static double SinkParTest_Dens_Contrast; // The density contrast between the
                                          // cloud and background
 static double SinkParTest_B0;            // The magnetic field
-static double
-    SinkParTest_theta_B; // The angle between the magnetic field and the z-axis
+static double SinkParTest_theta_B;  // The angle between the magnetic field and the z-axis
 static double SinkParTest_Mach_num; // The Mach number of the turbulence
-static char Tur_Table[MAX_STRING];  // The turbulence table
+static char Tur_Table[MAX_STRING]; // The turbulence table
 static double SinkParTest_Cs;       // sound spped
 static double SinkParTest_Rho0;     // The density of the cloud
 
@@ -34,7 +33,7 @@ double SinkParTest_rho_AD;   // The adiabatic density threshold
 // =======================================================================================
 
 #if (EOS == EOS_USER)
-void EoS_Init_Barotropic_SinkParTest();
+  void EoS_Init_Barotropic_SinkParTest();
 #endif
 
 //-------------------------------------------------------------------------------------------------------
@@ -53,40 +52,40 @@ void Validate() {
   if (MPI_Rank == 0)
     Aux_Message(stdout, "   Validating test problem %d ...\n", TESTPROB_ID);
 
-#if (MODEL != HYDRO)
+# if (MODEL != HYDRO)
   Aux_Error(ERROR_INFO, "MODEL != HYDRO !!\n");
-#endif
+# endif
 
-#ifndef GRAVITY
+# ifndef GRAVITY
   Aux_Error(ERROR_INFO, "GRAVITY must be enabled !!\n");
-#endif
+# endif
 
-#ifndef STAR_FORMATION
+# ifndef STAR_FORMATION
   Aux_Error(ERROR_INFO, "STAR_FORMATION must be enabled !!\n");
-#endif
+# endif
 
-#ifndef FEEDBACK
+# ifndef FEEDBACK
   Aux_Error(ERROR_INFO, "FEEDBACK must be enabled !!\n");
-#endif
+# endif
 
-#ifndef PARTICLE
+# ifndef PARTICLE
   Aux_Error(ERROR_INFO, "PARTICLE must be enabled !!\n");
-#endif
+# endif
 
-#ifndef MHD
+# ifndef MHD
   Aux_Error(ERROR_INFO, "MHD must be enabled !!\n");
-#endif
+# endif
 
-#if (EOS != EOS_USER)
+# if (EOS != EOS_USER)
   Aux_Error(ERROR_INFO, "EOS != EOS_USER !!\n");
-#endif
+# endif
 
-#ifdef PARTICLE
+# ifdef PARTICLE
   if (OPT__INIT == INIT_BY_FUNCTION && amr->Par->Init != PAR_INIT_BY_FUNCTION)
     Aux_Error(ERROR_INFO, "please set PAR_INIT = 1 (by FUNCTION) !!\n");
-#endif
+# endif
 
-#ifdef GRAVITY
+# ifdef GRAVITY
   if (!OPT__SELF_GRAVITY)
     Aux_Error(ERROR_INFO, "must enable OPT__SELF_GRAVITY !!\n");
 
@@ -95,24 +94,20 @@ void Validate() {
 
   if (OPT__EXT_POT)
     Aux_Error(ERROR_INFO, "must disable OPT__EXT_POT !!\n");
-#endif
+# endif
 
   if (MPI_Rank == 0) {
-#ifndef DUAL_ENERGY
-    Aux_Message(
-        stderr,
-        "WARNING : it's recommended to enable DUAL_ENERGY for this test\n");
-#endif
+    #ifndef DUAL_ENERGY
+      Aux_Message(stderr, "WARNING : it's recommended to enable DUAL_ENERGY for this test\n");
+    #endif
 
     for (int f = 0; f < 6; f++)
-      if (amr->BoxSize[0] != amr->BoxSize[1] ||
-          amr->BoxSize[0] != amr->BoxSize[2])
+      if (amr->BoxSize[0] != amr->BoxSize[1] || amr->BoxSize[0] != amr->BoxSize[2])
         Aux_Error(ERROR_INFO, "simulation domain is not cubic !!\n");
   }
 
   if (MPI_Rank == 0)
-    Aux_Message(stdout, "   Validating test problem %d ... done\n",
-                TESTPROB_ID);
+    Aux_Message(stdout, "   Validating test problem %d ... done\n",TESTPROB_ID);
 
 } // FUNCTION : Validate
 
@@ -144,21 +139,19 @@ void Validate() {
 //
 // Return      :  None
 //-------------------------------------------------------------------------------------------------------
-void LoadInputTestProb(const LoadParaMode_t load_mode, ReadPara_t *ReadPara,
-                       HDF5_Output_t *HDF5_InputTest) {
+void LoadInputTestProb(const LoadParaMode_t load_mode, ReadPara_t *ReadPara, HDF5_Output_t *HDF5_InputTest) 
+{
 
-#ifndef SUPPORT_HDF5
+# ifndef SUPPORT_HDF5
   if (load_mode == LOAD_HDF5_OUTPUT)
-    Aux_Error(ERROR_INFO, "please turn on SUPPORT_HDF5 in the Makefile for "
-                          "load_mode == LOAD_HDF5_OUTPUT !!\n");
-#endif
+    Aux_Error( ERROR_INFO, "please turn on SUPPORT_HDF5 in the Makefile for load_mode == LOAD_HDF5_OUTPUT !!\n" );
+# endif
 
   if (load_mode == LOAD_READPARA && ReadPara == NULL)
-    Aux_Error(ERROR_INFO,
-              "load_mode == LOAD_READPARA and ReadPara == NULL !!\n");
+    Aux_Error( ERROR_INFO, "load_mode == LOAD_READPARA and ReadPara == NULL !!\n" );
+
   if (load_mode == LOAD_HDF5_OUTPUT && HDF5_InputTest == NULL)
-    Aux_Error(ERROR_INFO,
-              "load_mode == LOAD_HDF5_OUTPUT and HDF5_InputTest == NULL !!\n");
+    Aux_Error( ERROR_INFO, "load_mode == LOAD_HDF5_OUTPUT and HDF5_InputTest == NULL !!\n" );
 
   // add parameters in the following format:
   // --> note that VARIABLE, DEFAULT, MIN, and MAX must have the same data type
@@ -209,8 +202,7 @@ void Load_Turbulence_SinkParTest() {
   double Total_VelZ_SQR = 0.0;
   double Vrms, Vrms_Scale; // used to rescale velocity
 
-  tur_table_NBin = Aux_LoadTable(tur_table, Tur_Table, 6, TargetCols,
-                                 RowMajor_No, AllocMem_Yes);
+  tur_table_NBin = Aux_LoadTable(tur_table, Tur_Table, 6, TargetCols, RowMajor_No, AllocMem_Yes);
 
   Table_VelX = tur_table + ColIdx_VelX * tur_table_NBin;
   Table_VelY = tur_table + ColIdx_VelY * tur_table_NBin;
@@ -231,19 +223,14 @@ void Load_Turbulence_SinkParTest() {
   }
 
   // Vrms = SQRT( ( Vx^2 + Vy^2 + Vz^2 ) / N + ( Vx + Vy + Vz / N) ^ 2 )
-  Vrms =
-      SQRT((Total_VelX_SQR + Total_VelY_SQR + Total_VelZ_SQR) / tur_table_NBin -
-           SQR((Total_VelX + Total_VelY + Total_VelZ) / tur_table_NBin));
+  Vrms = SQRT( (Total_VelX_SQR + Total_VelY_SQR + Total_VelZ_SQR) / tur_table_NBin - SQR((Total_VelX + Total_VelY + Total_VelZ) / tur_table_NBin));
   Vrms_Scale = SinkParTest_Mach_num * SinkParTest_Cs / Vrms;
 
   // Rescale velocity
   for (int i = 0; i < tur_table_NBin; i++) {
-    Table_Rescaled_VelX[i] =
-        Vrms_Scale * (Table_VelX[i] - Total_VelX / tur_table_NBin);
-    Table_Rescaled_VelY[i] =
-        Vrms_Scale * (Table_VelY[i] - Total_VelY / tur_table_NBin);
-    Table_Rescaled_VelZ[i] =
-        Vrms_Scale * (Table_VelZ[i] - Total_VelZ / tur_table_NBin);
+    Table_Rescaled_VelX[i] = Vrms_Scale * (Table_VelX[i] - Total_VelX / tur_table_NBin);
+    Table_Rescaled_VelY[i] = Vrms_Scale * (Table_VelY[i] - Total_VelY / tur_table_NBin);
+    Table_Rescaled_VelZ[i] = Vrms_Scale * (Table_VelZ[i] - Total_VelZ / tur_table_NBin);
   }
 
   delete[] tur_table;
@@ -289,16 +276,13 @@ void SetParameter() {
 
   if ((size - 1 < NX0_TOT[0]) || (size - 1 < NX0_TOT[1]) ||
       (size - 1 < NX0_TOT[2]))
-    Aux_Error(ERROR_INFO,
-              "size - 1 should be smaller than half of NX0_TOT_X/Y/Z !!\n");
+    Aux_Error(ERROR_INFO, "size - 1 should be smaller than half of NX0_TOT_X/Y/Z !!\n");
 
   // (2) set the problem-specific derived parameters
   SinkParTest_Core_Mass *= Const_Msun / UNIT_M;
-  SinkParTest_Cs = SQRT((Const_kB * ISO_TEMP / UNIT_E) /
-                        (MOLECULAR_WEIGHT * Const_amu / UNIT_M));
+  SinkParTest_Cs = SQRT((Const_kB * ISO_TEMP / UNIT_E) / (MOLECULAR_WEIGHT * Const_amu / UNIT_M));
   SinkParTest_R0 /= UNIT_L;
-  SinkParTest_Rho0 =
-      3.0 * SinkParTest_Core_Mass / (4.0 * M_PI * CUBE(SinkParTest_R0));
+  SinkParTest_Rho0 = 3.0 * SinkParTest_Core_Mass / (4.0 * M_PI * CUBE(SinkParTest_R0));
   SinkParTest_Omega0 /= 1 / UNIT_T;
   SinkParTest_rho_AD /= UNIT_D;
   SinkParTest_theta_B = SinkParTest_theta_B * M_PI / 180; // degree to radian
@@ -321,31 +305,20 @@ void SetParameter() {
 
   // (4) make a note
   if (MPI_Rank == 0) {
-    Aux_Message(stdout, "======================================================"
-                        "=======================\n");
-    Aux_Message(stdout, "  test problem ID       = %d\n", TESTPROB_ID);
-    Aux_Message(stdout, "  Sound speed           = %13.7e km/s\n",
-                SinkParTest_Cs * UNIT_V / Const_km);
-    Aux_Message(stdout, "  R0                    = %13.7e cm\n",
-                SinkParTest_R0 * UNIT_L);
-    Aux_Message(stdout, "  Rho0                  = %13.7e g/cm3\n",
-                SinkParTest_Rho0 * UNIT_D);
-    Aux_Message(stdout, "  Omega0                = %13.7e /s\n",
-                SinkParTest_Omega0 * UNIT_T);
-    Aux_Message(stdout, "  Core Mass             = %13.7e M_sun\n",
-                SinkParTest_Core_Mass / Const_Msun);
-    Aux_Message(stdout, "  B field               = %13.7e G\n", SinkParTest_B0);
-    Aux_Message(stdout, "  B angle               = %13.7e radian\n",
-                SinkParTest_theta_B);
-    Aux_Message(stdout, "  Mach number           = %13.7e \n",
-                SinkParTest_Mach_num);
-    Aux_Message(stdout, "  Turbulence table      = %s\n", Tur_Table);
-    Aux_Message(stdout, "  Iso_Temp              = %13.7e K\n",
-                SinkParTest_Iso_Temp);
-    Aux_Message(stdout, "  rho_AD                = %13.7e g/cm3\n",
-                SinkParTest_rho_AD);
-    Aux_Message(stdout, "======================================================"
-                        "=======================\n");
+    Aux_Message(stdout, "=======================================================================\n");
+    Aux_Message(stdout, "  test problem ID       = %d\n",             TESTPROB_ID);
+    Aux_Message(stdout, "  Sound speed           = %13.7e km/s\n",    SinkParTest_Cs * UNIT_V / Const_km);
+    Aux_Message(stdout, "  R0                    = %13.7e cm\n",      SinkParTest_R0 * UNIT_L);
+    Aux_Message(stdout, "  Rho0                  = %13.7e g/cm3\n",   SinkParTest_Rho0 * UNIT_D);
+    Aux_Message(stdout, "  Omega0                = %13.7e /s\n",      SinkParTest_Omega0 * UNIT_T);
+    Aux_Message(stdout, "  Core Mass             = %13.7e M_sun\n",   SinkParTest_Core_Mass / Const_Msun);
+    Aux_Message(stdout, "  B field               = %13.7e G\n",       SinkParTest_B0);
+    Aux_Message(stdout, "  B angle               = %13.7e radian\n",  SinkParTest_theta_B);
+    Aux_Message(stdout, "  Mach number           = %13.7e \n",        SinkParTest_Mach_num);
+    Aux_Message(stdout, "  Turbulence table      = %s\n",             Tur_Table);
+    Aux_Message(stdout, "  Iso_Temp              = %13.7e K\n",       SinkParTest_Iso_Temp);
+    Aux_Message(stdout, "  rho_AD                = %13.7e g/cm3\n",   SinkParTest_rho_AD);
+    Aux_Message(stdout, "=======================================================================\n");
   }
 
   if (MPI_Rank == 0)
@@ -376,8 +349,8 @@ void SetParameter() {
 //
 // Return      :  fluid
 //-------------------------------------------------------------------------------------------------------
-void SetGridIC(real fluid[], const double x, const double y, const double z,
-               const double Time, const int lv, double AuxArray[]) {
+void SetGridIC(real fluid[], const double x, const double y, const double z, const double Time, const int lv, double AuxArray[]) 
+{
   const double dx = x - amr->BoxCenter[0];
   const double dy = y - amr->BoxCenter[1];
   const double dz = z - amr->BoxCenter[2];
@@ -404,24 +377,20 @@ void SetGridIC(real fluid[], const double x, const double y, const double z,
   VelZ = Table_Rescaled_VelZ[index];
 
   if (Rs < SinkParTest_R0) {
-    Dens = SinkParTest_Rho0 *
-           (1 + SinkParTest_Delta_Dens * COS(2 * ATAN(dy / dx)));
+    Dens = SinkParTest_Rho0 * (1 + SinkParTest_Delta_Dens * COS(2 * ATAN(dy / dx)));
     VelX -= SinkParTest_Omega0 * dy;
     VelY += SinkParTest_Omega0 * dx;
   } else {
     Dens = SinkParTest_Rho0 / SinkParTest_Dens_Contrast;
   }
 
-  Pres = EoS_DensTemp2Pres_CPUPtr(Dens, ISO_TEMP, NULL, EoS_AuxArray_Flt,
-                                  EoS_AuxArray_Int, h_EoS_Table);
-  Eint = EoS_DensPres2Eint_CPUPtr(Dens, Pres, NULL, EoS_AuxArray_Flt,
-                                  EoS_AuxArray_Int, h_EoS_Table);
+  Pres = EoS_DensTemp2Pres_CPUPtr(Dens, ISO_TEMP, NULL, EoS_AuxArray_Flt, EoS_AuxArray_Int, h_EoS_Table);
+  Eint = EoS_DensPres2Eint_CPUPtr(Dens, Pres, NULL, EoS_AuxArray_Flt, EoS_AuxArray_Int, h_EoS_Table);
 
   MomX = Dens * VelX;
   MomY = Dens * VelY;
   MomZ = Dens * VelZ;
-  Etot = Hydro_ConEint2Etot(Dens, MomX, MomY, MomZ, Eint,
-                            0.0); // do NOT include magnetic energy here
+  Etot = Hydro_ConEint2Etot(Dens, MomX, MomY, MomZ, Eint, 0.0); // do NOT include magnetic energy here
 
   fluid[DENS] = Dens;
   fluid[MOMX] = MomX;
@@ -449,9 +418,8 @@ void SetGridIC(real fluid[], const double x, const double y, const double z,
 //
 // Return      :  magnetic
 //-------------------------------------------------------------------------------------------------------
-void SetBFieldIC(real magnetic[], const double x, const double y,
-                 const double z, const double Time, const int lv,
-                 double AuxArray[]) {
+void SetBFieldIC(real magnetic[], const double x, const double y, const double z, const double Time, const int lv, double AuxArray[]) 
+{
   double MagX = 0.0, MagY = 0.0, MagZ = 0.0;
   MagZ = SinkParTest_B0 * COS(SinkParTest_theta_B);
   MagY = SinkParTest_B0 * SIN(SinkParTest_theta_B);
@@ -465,12 +433,12 @@ void SetBFieldIC(real magnetic[], const double x, const double y,
 #endif // #if ( MODEL == HYDRO )
 
 #ifdef PARTICLE
-void Par_Init_ByFunction_SinkParTest(
-    const long NPar_ThisRank, const long NPar_AllRank, real_par *ParMass,
-    real_par *ParPosX, real_par *ParPosY, real_par *ParPosZ, real_par *ParVelX,
-    real_par *ParVelY, real_par *ParVelZ, real_par *ParTime, long_par *ParType,
-    real_par *AllAttributeFlt[PAR_NATT_FLT_TOTAL],
-    long_par *AllAttributeInt[PAR_NATT_INT_TOTAL]) {
+void Par_Init_ByFunction_SinkParTest(const long NPar_ThisRank, const long NPar_AllRank, real_par *ParMass,
+                                     real_par *ParPosX, real_par *ParPosY, real_par *ParPosZ, real_par *ParVelX,
+                                     real_par *ParVelY, real_par *ParVelZ, real_par *ParTime, long_par *ParType,
+                                     real_par *AllAttributeFlt[PAR_NATT_FLT_TOTAL],
+                                     long_par *AllAttributeInt[PAR_NATT_INT_TOTAL]) 
+{
   // we only use star particles, so keep here empty.
 } // FUNCTION : Par_Init_ByFunction
 #endif
@@ -493,31 +461,27 @@ void Init_TestProb_Hydro_SinkParTest() {
   // validate the compilation flags and runtime parameters
   Validate();
 
-#if (MODEL == HYDRO)
+# if (MODEL == HYDRO)
   // set the problem-specific runtime parameters
   SetParameter();
 
   // set the function pointers of various problem-specific routines
   Init_Function_User_Ptr = SetGridIC;
-#ifdef MHD
+# ifdef MHD
   Init_Function_BField_User_Ptr = SetBFieldIC;
-#endif
-#endif // #if ( MODEL == HYDRO )
+# endif
+# endif // #if ( MODEL == HYDRO )
 
-#ifdef PARTICLE
-  Par_Init_ByFunction_Ptr =
-      Par_Init_ByFunction_SinkParTest; // option: PAR_INIT=1; example:
-                                       // Particle/Par_Init_ByFunction.cpp
-#endif
-#if (EOS == EOS_USER)
-  EoS_Init_Ptr =
-      EoS_Init_Barotropic_SinkParTest; // option: EOS in the Makefile; example:
-                                       // EoS/User_Template/CPU_EoS_User_Template.cpp
+# ifdef PARTICLE
+  Par_Init_ByFunction_Ptr = Par_Init_ByFunction_SinkParTest; // option: PAR_INIT=1; example: Particle/Par_Init_ByFunction.cpp
+# endif
+# if (EOS == EOS_USER)
+  EoS_Init_Ptr = EoS_Init_Barotropic_SinkParTest; // option: EOS in the Makefile; example: EoS/User_Template/CPU_EoS_User_Template.cpp
   EoS_End_Ptr = NULL;
-#endif
-#ifdef SUPPORT_HDF5
+# endif
+# ifdef SUPPORT_HDF5
   Output_HDF5_InputTest_Ptr = LoadInputTestProb;
-#endif
+# endif
 
   if (MPI_Rank == 0)
     Aux_Message(stdout, "%s ... done\n", __FUNCTION__);
